4008f93f7f254b0de0e7e080eb9728343d3caf42
Danail Irinchev <irinchev@gmail.com>
2021-06-23 22:22:06 +0200

    add jenkinsfile

--- src/main/scala/my/test/Routes.scala
+++ src/main/scala/my/test/Routes.scala
@@ -6,7 +6,7 @@ import akka.http.scaladsl.server.Directives.{complete, get, path}
 trait Routes {
   val routes = path("hello") {
     get {
-      complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>"))
+      complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http!</h1>"))
     }
   }
 }
